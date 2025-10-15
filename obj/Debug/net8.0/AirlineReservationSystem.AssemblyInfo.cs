2bddb92e7ec22a0455385a0618733d91215ca729
Vincent <vincenttayros078@gmail.com>
2025-10-15 17:36:08 +0800

    latest

--- obj/Debug/net8.0/AirlineReservationSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/AirlineReservationSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AirlineReservationSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f5affb3e0a8c0d741113fa62870463f95f4882a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4225cb6af9bc1a2a56f8b1581ac05eb155527fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("AirlineReservationSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AirlineReservationSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
