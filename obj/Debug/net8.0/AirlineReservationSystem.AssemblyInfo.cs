77832b51163f2c5a0457243375ea0f7b760796eb
Vincent <vincenttayros078@gmail.com>
2025-11-03 22:43:33 +0800

    added fixed admin ui

--- obj/Debug/net8.0/AirlineReservationSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/AirlineReservationSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AirlineReservationSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce046629ebebbd7de14760c532b607b03d0b8bb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80b51ca9b605142c443fd20b63c66fdd33f0c5b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("AirlineReservationSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AirlineReservationSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
